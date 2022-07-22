8d22f4d542ccf8a137518cc7b9a2cd1a680e57c4
Cybaries <amankantjha@gmail.com>
2022-07-22 15:05:28 +0530

    Added Background and Search Bar

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.scss'
+function Main() {
+  return (
+    <div class="container">
+      <div className="form">
+        <input type="text" placeholder='Search'/>
+        <button className="fa fa-search"></button>
+      </div>
+    </div>
+  );
+}
+
+export default Main
