8199dd45c62fdc389d495e5c03edd19cb1ff93e0
Cybaries <amankantjha@gmail.com>
2022-07-23 21:29:24 +0530

    made the recommendation invisible on clicking outside

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,39 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import './Main.scss'
 import logo from '../images/Logo.png'
 import Trend from './Trend';
+// import P2 from './P2';
 function Main() {
-  const [isShown, setIsShown] = useState(false);
+  const [ isShown, setIsShown ] = useState(false);
+  // const [ isDisplay, setDisplay ] = useState(false);
+  const [ message, setMessage ] = useState('');
+  const update = event => { 
+    setMessage(event.target.value);
+  }
   const handleClick = event => {
-    setIsShown(current => !current);
+    event.preventDefault();
+    setIsShown(true);
   };
+  const invisible = () => {
+    setIsShown(false);
+  }
+  const search = event => { 
+    event.preventDefault();
+    window.open(`https://www.google.com/search?q=${message}`, '_self', 'noopener, noreferrer');
+    // setDisplay(!isDisplay);
+  }
+
   return (
     <div class="container" id='container'>
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
       <div className="form">
-        <input type="text" placeholder='Search' onClick={handleClick}/>
-        <button className="fa fa-search"></button>
+        <input type="text" name='message' placeholder='Search' value={message} onChange={update} onClick={handleClick} onBlur={invisible} />
+        <button className="fa fa-search" onClick={search}></button>
       </div>
       {isShown && <Trend />}
+      {/* {isDisplay && <P2 />} */}
     </div>
   );
 }
